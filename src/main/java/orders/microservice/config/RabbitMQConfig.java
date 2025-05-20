437c4b4922040f1b7733632f4d054fd10bc81aa1
Gabriel Gomes de Amorim Silva <gabrielgomes@MacBook-Air-de-Gabriel.local>
2025-05-19 23:13:49 -0300

    feat: add rabbitMq config

---
+++ src/main/java/orders/microservice/config/RabbitMQConfig.java
@@ -0,0 +1,21 @@
+package orders.microservice.config;
+
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    public static final String ORDER_CREATED_QUEUE = "orders-microservice";
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+    @Bean
+    public Declarable orderCreatedQueue() {
+        return new Queue(ORDER_CREATED_QUEUE);
+    }
+}
