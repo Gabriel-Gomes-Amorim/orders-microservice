1d5cb34ae9527609a46228e24919ce60d3430ff1
Gabriel Gomes de Amorim Silva <gabrielgomes@MacBook-Air-de-Gabriel.local>
2025-05-19 21:59:47 -0300

    init

---
+++ src/main/java/orders/microservice/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package orders.microservice.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
