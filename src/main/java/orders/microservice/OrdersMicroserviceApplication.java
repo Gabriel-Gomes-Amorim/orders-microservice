28aba587aca96a68814e1035011eb9a0981b55a2
Gabriel Gomes de Amorim Silva <gabrielgomes@MacBook-Air-de-Gabriel.local>
2025-05-19 23:13:33 -0300

    refactor

--- src/main/java/orders/microservice/OrdersMicroserviceApplication.java
+++ src/main/java/orders/microservice/OrdersMicroserviceApplication.java
@@ -1,13 +1,11 @@
-package orders.microservice.demo;
+package orders.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class OrdersMicroserviceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(OrdersMicroserviceApplication.class, args);
 	}
-
 }
